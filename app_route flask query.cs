bf1162ac9bc09b4489d4540155cba706d9233c37
Ravid-CheckMarx <84515813+Ravid-CheckMarx@users.noreply.github.com>
2021-10-13 13:18:43 +0300

    Update app_route flask query.cs

--- app_route flask query.cs
+++ app_route flask query.cs
@@ -1,3 +1,4 @@
+/* Creating a node */
 Func<string, CxList> createCommentNode = (text) => {
 	Comment root = new Comment();
 	Comment comment = new Comment(text, text, root);
@@ -12,6 +13,7 @@ CxList customAttributes = Find_CustomAttribute();
 CxList methods = Find_Methods();
 CxList flaskApp = Find_Methods_By_Import("flask", new string[] {"Flask"}).GetAssignee();
 string flaskAppVar = flaskApp.GetName();
+/* Regex for getting the render template */
 CxList render_source = All.FindByRegexExt(@"(?<=render_template\([''""])(\w+\.[a-z]{3,})(?=[''""],?.+\))");
 
 CxList appRoute = customAttributes.FindByCustomAttribute(flaskAppVar + ".route");
@@ -22,16 +24,13 @@ CxList appRoute = customAttributes.FindByCustomAttribute(flaskAppVar + ".route")
 CxList route = methods.FindByShortName("route");
 CxList flk = methods.FindByShortName("Flask").GetAssignee();
 CxList app = All.FindAllReferences(flk).GetMembersOfTarget();
-	
-/*CxList parameter = All.GetParameters(fun);*/
 
-/*CxList app_routes_methods = Find_ArrayInitializer().GetByAncs(appRoute);*/
-
-foreach (CxList ar in appRoute) {
-	
-	
+/* Running throw each route */
+foreach (CxList ar in appRoute) 
+{
 	CxList path = ar;
 	CxList allUrls_andRoutes = All.GetByAncs(ar);
+	/* Extracting the method of a route*/
 	CxList fun = All.GetMethod(ar);
 	
 	/* urls of decorations */
@@ -46,27 +45,29 @@ foreach (CxList ar in appRoute) {
 		cxLog.WriteDebugMessage(bb);
 	}*/	
 
-	CxList appRouteMethods = allUrls_andRoutes.FindByShortName("methods");
-	
 	/* HTTP type of decorations */
 	
+	/* Getch the array of the HTTP types */
 	CxList getHttpType = Find_ArrayInitializer().GetByAncs(ar).CxSelectElements<ArrayInitializer>(x => x.InitialValues);
 	
-	
 	path = path.ConcatenatePath(createCommentNode("HTTP types"), false);
+	/* Fetch only routes that has the method=[] in it */
 	CxList app_routes_methods = Find_ArrayInitializer().GetByAncs(ar);
 	if (app_routes_methods.Count > 0)
 	{
 		foreach (CxList type in getHttpType)
 		{
+			/*Returning all HTTP types */
 			path = path.ConcatenatePath(type, false);
 		}
 	}
 	else 
+		/* In case no types mentioned, the defualt is GET */
 		path = path.ConcatenatePath(createCommentNode("GET"), false);
 	
 	
 	path = path.ConcatenatePath(createCommentNode("method"), false);
+	/* Return the method attached to each route */
 	path = path.ConcatenatePath(fun, false);
 	
 	
@@ -115,7 +116,14 @@ foreach (CxList ar in appRoute) {
 		path = path.ConcatenatePath(createCommentNode("input parameters"), false);
 		path = path.ConcatenatePath(createCommentNode("no parameters"), false);
 	}
-	
+	/* trying to fetch the rendered page that the fuction returns (in case there is one) */
+/*	var viewCallLine = Find_ViewCalls().GetByAncs(ar.GetAncOfType<MethodDecl>()).CxSelectElementValues<ViewCall,int>(x => x.LinePragma.Line && x.LinePragma.Filename == ar.LinePragma.Filename).FirstOrDefault();
+
+	if (viewCallLine != null)
+	{
+		result.Add(render_source.FindByPosition(viewCallLine));
+	}
+*/
 	result.Add(path);
 }
 
