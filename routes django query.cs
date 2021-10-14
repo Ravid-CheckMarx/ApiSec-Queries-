fb646bfab44b3e7e3153e77d1cd7ad94abcc20cb
Ravid-CheckMarx <84515813+Ravid-CheckMarx@users.noreply.github.com>
2021-10-14 11:55:07 +0300

    added String.Slpit to get param type and param name

--- routes django query.cs
+++ routes django query.cs
@@ -12,12 +12,18 @@ Func<string, CxList> createCommentNode = (text) => {
 /* Find all method invoke expr */
 CxList routes = Find_ArrayInitializer().CxSelectElements<ArrayInitializer>(x => x.InitialValues).FindByType<MethodInvokeExpr>();
 
+CxList functions = All.FindByType(typeof(MemberAccess));
+
+
 foreach (CxList route in routes)
 {
+	
 	/* run throw all method invoke expr and filter the urls, paths and re_paths  */
 	if ((route == route.FindByName("url")) || (route == route.FindByName("path")) || (route == route.FindByName("re_path")))
 	{
 		CxList path = route;
+		
+		
 		/* Get all the data for each route */
 		CxList allUrls_andRoutes = All.GetByAncs(route);
 	
@@ -37,32 +43,41 @@ foreach (CxList route in routes)
 			{
 				path = path.ConcatenatePath(createCommentNode("endpoint"), false);
 				path = path.ConcatenatePath(end, false);
-				/* Check param type, in case there is one */
-				if (end.GetName().Contains("<str:") == true)
+				
+				string str_url = end.GetName();
+				char[] delimiterChars = {'/'};
+				string[] words = str_url.Split(delimiterChars);
+
+				foreach (var word in words)
 				{
-					path = path.ConcatenatePath(createCommentNode("param type"), false);
-					path = path.ConcatenatePath(createCommentNode("str"), false);
-				}
-				else 
-					if (end.GetName().Contains("<float:") == true)
-				{
-					path = path.ConcatenatePath(createCommentNode("param type"), false);
-					path = path.ConcatenatePath(createCommentNode("float"), false);
-				}
-			
-				else 
-					if (end.GetName().Contains("<path:") == true)
-				{
-					path = path.ConcatenatePath(createCommentNode("param type"), false);
-					path = path.ConcatenatePath(createCommentNode("path"), false);
-				}
-				else 
-					if (end.GetName().Contains("<int:") == true)
-				{
-					path = path.ConcatenatePath(createCommentNode("param type"), false);
-					path = path.ConcatenatePath(createCommentNode("int"), false);
+					/* Check if we reached the <type:parameter name> pair in the URL */
+					if (word.StartsWith("<"))
+					{
+						/* Split between the type and the param name */
+						string[] par_type_and_name = word.Split(':');
+						foreach (var par in par_type_and_name)
+						{
+							/* Check if this is the type */
+							if (par.StartsWith("<"))
+							{
+								/* Remove unnessecary chars */
+								var trimmed_par = par.Trim('<');
+								path = path.ConcatenatePath(createCommentNode("param type"), false);
+								path = path.ConcatenatePath(createCommentNode(trimmed_par), false);
+							}
+								/* Check if this is the name */
+								if (par.EndsWith(">"))
+							{
+								/* Remove unnessecary chars */
+								var trimmed_par = par.Trim('>');
+								path = path.ConcatenatePath(createCommentNode("param name"), false);
+								path = path.ConcatenatePath(createCommentNode(trimmed_par), false);
+							}
+						}
+					}
 				}
 			}
+		
 			/* Will happen after we finished iterating the endpoint */
 			if (getendpoint == false)
 			{
@@ -81,23 +96,24 @@ foreach (CxList route in routes)
 			getendpoint = false;
 			
 		}
-			/* Getting the view as "views.index" for example */		
-			if (member.Count > 0)
+		
+		/* Getting the view as "views.index" for example */		
+		if (member.Count > 0)
+		{
+			foreach (CxList mem in member)
 			{
-				foreach (CxList mem in member)
-				{
-					path = path.ConcatenatePath(mem, false);
-				}
+				path = path.ConcatenatePath(mem, false);
 			}
-			else 
-				/* Getting the view as "user.login" for example after it got imported (from loginas.views import user_login) */	
-				if (UnknownRef.Count > 0)
+		}
+		else 
+			/* Getting the view as "user.login" for example after it got imported (from loginas.views import user_login) */	
+			if (UnknownRef.Count > 0)
+		{
+			foreach (CxList unknown in UnknownRef)
 			{
-				foreach (CxList unknown in UnknownRef)
-				{
-					path = path.ConcatenatePath(unknown, false);
-				}
+				path = path.ConcatenatePath(unknown, false);
 			}
+		}
 	
 		result.Add(path);
 	}
